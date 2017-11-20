434b578c414742c0efe5b29c4f2d1479c0286c67
Abdul <aghafoor@tssg.org>
2017-11-20 15:47:09 +0000

    Small change to check if jenkins is running.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Going for deployment on Jenkins (Continous Integration)");
+  res.send("== Going for deployment on Jenkins (Continous Integration) ==");
 });
 
 app.listen(4000);
